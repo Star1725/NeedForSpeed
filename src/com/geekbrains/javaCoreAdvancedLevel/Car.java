404efefa97eb11da7823ecf91e8c95f6c4354b45
Sergey <starovoytov1725@gmail.com>
2020-10-03 13:13:02 +0300

    реализовано:  - ожидание гонщиков друг друга  - одновременный старт  - корректный вывод "Гонка началась" и "Гонка закончилась"

--- src/com/geekbrains/javaCoreAdvancedLevel/Car.java
+++ src/com/geekbrains/javaCoreAdvancedLevel/Car.java
@@ -1,10 +1,8 @@
 package com.geekbrains.javaCoreAdvancedLevel;
 
-import java.util.concurrent.CyclicBarrier;
-
 public class Car implements Runnable {
     private static int CARS_COUNT;
-    private static CyclicBarrier cyclicBarrier;
+
     static {
         CARS_COUNT = 0;
     }
@@ -21,7 +19,6 @@ public class Car implements Runnable {
         this.race = race;
         this.speed = speed;
         CARS_COUNT++;
-        cyclicBarrier = new CyclicBarrier(CARS_COUNT);
         this.name = "Участник #" + CARS_COUNT;
     }
     @Override
@@ -30,12 +27,15 @@ public class Car implements Runnable {
             System.out.println(this.name + " готовится");
             Thread.sleep(500 + (int)(Math.random() * 800));
             System.out.println(this.name + " готов");
-            cyclicBarrier.await();
+            MainClass.getCountDownLatchStart().countDown();
+            MainClass.getCyclicBarrier().await();
+
         } catch (Exception e) {
             e.printStackTrace();
         }
         for (int i = 0; i < race.getStages().size(); i++) {
             race.getStages().get(i).go(this);
         }
+        MainClass.getCountDownLatchFinish().countDown();
     }
 }
--- src/com/geekbrains/javaCoreAdvancedLevel/MainClass.java
+++ src/com/geekbrains/javaCoreAdvancedLevel/MainClass.java
@@ -1,20 +1,53 @@
 package com.geekbrains.javaCoreAdvancedLevel;
 
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.CyclicBarrier;
 
 public class MainClass {
     public static final int CARS_COUNT = 4;
+
+    public static CountDownLatch getCountDownLatchStart() {
+        return countDownLatchStart;
+    }
+
+    private static CountDownLatch countDownLatchStart;
+
+    public static CountDownLatch getCountDownLatchFinish() {
+        return countDownLatchFinish;
+    }
+
+    private static CountDownLatch countDownLatchFinish;
+
+    public static CyclicBarrier getCyclicBarrier() {
+        return cyclicBarrier;
+    }
+
+    private static CyclicBarrier cyclicBarrier;
+
     public static void main(String[] args) {
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
         Race race = new Race(new Road(60), new Tunnel(), new Road(40));
+        countDownLatchFinish = new CountDownLatch(CARS_COUNT);
         Car[] cars = new Car[CARS_COUNT];
+        countDownLatchStart = new CountDownLatch(CARS_COUNT);
+        cyclicBarrier = new CyclicBarrier(CARS_COUNT);
         for (int i = 0; i < cars.length; i++) {
             cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
         }
         for (int i = 0; i < cars.length; i++) {
             new Thread(cars[i]).start();
         }
+        try {
+            countDownLatchStart.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+        try {
+            countDownLatchFinish.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
     }
 }
